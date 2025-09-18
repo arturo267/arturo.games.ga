4adefff79a7c6573404d5b81a5cc878a274e7a22
arturo267 <130789993+arturo267@users.noreply.github.com>
2025-09-18 13:47:02 -0500

    Update startup.js

--- js/startup.js
+++ js/startup.js
@@ -1,1 +1,19 @@
-
+ if (localStorage.getItem("NotFirstTime") === null) {
+        // This is the first time the user is visiting, so set default values
+        
+        // Default colors for palette 1
+        localStorage.setItem("R1", "255");
+        localStorage.setItem("G1", "0");
+        localStorage.setItem("B1", "0");
+        
+        // Default colors for palette 2
+        localStorage.setItem("R2", "230");
+        localStorage.setItem("G2", "230");
+        localStorage.setItem("B2", "50");
+        
+        // Default colors for palette 3
+        localStorage.setItem("R3", "0");
+        localStorage.setItem("G3", "0");
+        localStorage.setItem("B3", "0");
+		localStorage.setItem("NotFirstTime", "true");
+    };
